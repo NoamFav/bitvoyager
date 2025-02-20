d928a34b69e1ea21947bbc22ac65079aeb21a2b5
noamfav <95364866+NoamFav@users.noreply.github.com>
2025-02-20 22:39:06 +0100

    i think it fixed keys issues

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,22 @@ import react from "@vitejs/plugin-react";
 import fs from "fs";
 import path from "path";
 
+// Check if running in development mode
+const isDev = process.env.NODE_ENV === "development";
+
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
-    },
-    headers: {
-      "Cross-Origin-Opener-Policy": "same-origin",
-      "Cross-Origin-Embedder-Policy": "require-corp",
-    },
-  },
-  base: "/bitjourney",
+  server: isDev
+    ? {
+        https: {
+          key: fs.readFileSync(path.resolve(__dirname, "localhost-key.pem")),
+          cert: fs.readFileSync(path.resolve(__dirname, "localhost.pem")),
+        },
+        headers: {
+          "Cross-Origin-Opener-Policy": "same-origin",
+          "Cross-Origin-Embedder-Policy": "require-corp",
+        },
+      }
+    : undefined, // No server settings for production
+  base: "/bitjourney", // Ensure correct base path for GitHub Pages
 });
